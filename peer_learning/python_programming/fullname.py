6cb03b7d083a8a02d36883c0db506a0817903843
unknown <danielzadva@gmail.com>
2025-03-23 20:22:47 +0100

    learning materials

---
+++ peer_learning/python_programming/fullname.py
@@ -0,0 +1,6 @@
+firstname = input("Enter your First Name: ")
+secondname = input("Enter your Second Name: ")
+fullname = firstname + " " + secondname
+print("Your full name is: ", fullname.title())
+
+# upper(), lower()
