1157294b4b698338d5773fda518f5ef91cc655c2
unknown <danielzadva@gmail.com>
2025-03-23 23:00:11 +0100

    square root of a number

---
+++ peer_learning/python_programming/squareroot.py
@@ -0,0 +1,7 @@
+# python program to compute square root of a number
+number = int(input("Enter a number: "))
+
+def squareroot(n):
+    return n ** 0.5
+
+print(f'The square root of {number} is = {squareroot(number)}')
